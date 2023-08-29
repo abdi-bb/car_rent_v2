74b97f44067db075674fdba6df1e06e25edcf3d4
Abdi Berhe <abdi201380@gmail.com>
2023-08-29 10:57:12 +0300

    [Add]: intial file

---
+++ car_app/schema.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS car;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS reservation;
+
+
+CREATE TABLE admin (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  username TEXT UNIQUE NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE car (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  admin_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  model TEXT NOT NULL,
+  status TEXT NOT NULL,
+  seat INTEGER NOT NULL,
+  door INTEGER NOT NULL,
+  gearbox TEXT NOT NULL,
+  image TEXT,
+  FOREIGN KEY (admin_id) REFERENCES admin (id),
+);
+
+CREATE TABLE customer (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  admin_id INTEGER NOT NULL,
+  username TEXT UNIQUE NOT NULL,
+  name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  address TEXT NOT NULL,
+  phone_number TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL,
+  FOREIGN KEY (admin_id) REFERENCES admin (id)
+);
+
+CREATE TABLE reservation (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  admin_id INTEGER NOT NULL,
+  customer_id INTEGER NOT NULL,
+  customer_name TEXT NOT NULL,
+  pickup_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dropoff_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customer (id),
+  FOREIGN KEY (admin_id) REFERENCES admin (id)
+);
