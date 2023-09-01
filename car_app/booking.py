631c3e38ab3e65cab6c37df62811ab6e9f01b757
Abdi Berhe <abdi201380@gmail.com>
2023-09-01 11:30:34 +0300

    [Update]: Updating the delete method of booking

--- car_app/booking.py
+++ car_app/booking.py
@@ -108,4 +108,4 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM booking WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('booking.index'))
